f4e5f324d7f75b5ed3bc2743ac137cae30158a2b
Paul <pgflynn@gmail.com>
2014-10-03 10:09:08 +1300

    basic setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,184 +1,45 @@
-
 module.exports = function(grunt) {
+    var pkg = grunt.file.readJSON('package.json');
 
-    var pkg = grunt.file.readJSON( __dirname +'/package.json');
-    var path = require('path');
-
-    // Project configuration.
     grunt.initConfig({
-
-        pkg: pkg,
+        pkg: grunt.file.readJSON('package.json'),
 
         /**
          * Set up the project environment
+         * --------------------------------------------------------------------
          */
         paths: {
-            site: '/www',
-            assets: '/src',
-            javascript: '<%= paths.assets %>/js',
-            minifiedJavascript: '<%= paths.javascript %>/dist',
-            images: '<%= paths.assets %>/images',
-            grunticons: '<%= paths.images %>/grunticons',
-            css: '<%= paths.assets %>/css',
-            build: 'build',
-            sass: '<%= paths.assets %>/sass'
+            src: "src",
+            assets: "www/assets",
+            grunticons: "<%= paths.assets %>/images/grunticons",
+            sass: "<%= paths.src %>/sass",
+            css: "<%= paths.assets %>/css"
         },
-
-      /**
-       * Check Grunt Dependencies
-       * --------------------------------------------------------------------
-       */
-      checkDependencies: {
-        this: {
-          options: {
-            npmInstall: false
-          }
-        }
-      },
-
-
-        /**
-         * Concat
-         */
-        // concat: {
-
-        //     lib: {
-        //         src: [
-        //             '<%= paths.javascript %>/lib/browser.js',
-        //            '<%= paths.javascript %>/lib/zepto.min.js',
-        //             '<%= paths.javascript %>/lib/jquery-1.11.1.min.js',
-        //             '<%= paths.javascript %>/lib/quicktube.js',
-        //             '<%= paths.javascript %>/lib/snap.svg-min.js',
-        //             '<%= paths.javascript %>/lib/fancy-inputs.js',
-        //             '<%= paths.javascript %>/lib/show-hide.js',
-        //             '<%= paths.javascript %>/lib/analytics.js'
-        //         ],
-        //         dest: '<%= paths.build %>/js/lib.js'
-        //     },
-
-        //     static_mappings: {
-        //         src: [
-        //             "<%= paths.build %>/js/lib.js",
-        //             "<%= paths.build %>/js/site.js"
-        //         ],
-        //         dest: '<%= paths.minifiedJavascript %>/site.js',
-        //         filter: 'isFile'
-        //     }
-
-        // },
-
-        /**
-         * Let's use some proper module structure for the public site, woohoo!
-         * */
-        // browserify: {
-        //     application: {
-        //         files: {
-        //             '<%= paths.build %>/js/site.js': [
-        //                 '<%= paths.javascript %>/components/**/*.js',
-        //                 '<%= paths.javascript %>/site.js'
-        //             ]
-        //         },
-        //     }
-        // },
-
-        /**
-         * Blast generated CSS and JS files.
-         */
-        // clean: {
-
-        //     js: [
-        //         "<%= paths.minifiedJavascript %>/**/*.js"
-        //     ],
-
-        //     build: {
-        //         src: ["<%= paths.build %>"]
-        //     }
-
-        // },
-
-        /**
-         * Minify the production Javascript
-         */
-        // uglify: {
-        //     options: {
-        //         banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n",
-        //         mangle: {
-        //             except: ["jQuery", "Backbone", "SL"]
-        //         },
-        //         files: [
-        //             {
-        //                 expand: true,
-        //                 flatten: true,   // remove all unnecessary nesting
-        //                 src: "<%= paths.minifiedJavascript %>/*.js",  // source files mask
-        //                 dest: "<%= paths.minifiedJavascript %>",    // destination folder
-        //                 ext: '.min.js'   // replace .js to .min.js
-        //             }
-        //         ]
-        //     },
-        //     dev: {
-        //         options: {
-        //             beautify: true
-
-        //         },
-        //         files: "<%= uglify.options.files %>"
-        //     },
-        //     dist: {
-        //         options: {
-        //             beautify: false
-        //         },
-        //         files: "<%= uglify.options.files %>"
-        //     }
-        // },
-
-        /**
-         * Lint the JS and hurt all the feelings!
-         */
-        // jshint: {
-        //     options: {
-        //         force: true,
-        //         curly: true,
-        //         eqeqeq: true,
-        //         eqnull: true,
-        //         browser: true,
-        //         globals: {
-        //             jQuery: true
-        //         },
-        //         // Ignore specific warnings - would be good to fix them instead of ignoring them...testing testing testing.
-        //         //'-W061': true,  // Eval can be harmful -
-        //         //'-W083': true,   // Don't make functions within a loop
-        //         ignores: [
-        //             "<%= paths.minifiedJavascript %>/*.js",
-        //             "<%= paths.javascript %>/lib/**/*.js",
-        //             "/**/*.min.js",
-        //             "/**/*.mini.js",
-        //             "/**/*_bak.js",
-        //             "/**/jquery.js",
-        //             "/**/jquery.*.js"
-        //         ]
-        //     },
-        //     all: ['<%= paths.javascript %>/**/*.js']
-        // },
-
-
-        sass: {
-            dist: {
+        watch: {
+            sass: {
+                options: {
+                    nospawn: true,
+                    livereload: true
+                },
                 files: [
-                    {
-                        expand: true,     // Enable dynamic expansion.
-                        cwd: '<%= paths.assets %>/sass/',      // Src matches are relative to this path.
-                        src: ['**/*.scss', "!**/_*.scss"], // Actual pattern(s) to match.
-                        dest: '<%= paths.site %>/assets/css/',   // Destination path prefix.
-                        ext: '.css'   // Dest filepaths will have this extension.
-                    }
+                    "<%= paths.sass %>/**/*.scss"
+                ],
+                tasks: [
+                    "css"
                 ]
-                ,options: {
+            }
+        },
+        sass: {
+            dev: {
+                files: {
+                    "www/assets/css/style.css": "<%= paths.sass %>/style.scss"
+                },
+                options: {
                     sourceComments: 'none',
-                    sourceMap: true
+                    sourceMap: false
                 }
             }
         },
-
-
         grunticon_pigment: {
 
             icons: {
@@ -191,169 +52,106 @@ module.exports = function(grunt) {
                 options: {
 
                     // colours for svg colourising
-                    svgColors: ["#ffffff", "#f9ba30", "#4aa1c5", "#3587a7"],
+                    // white, blue, dark-blue, grey, dark-grey, green, orange, red
+                    svgColors: ["#ffffff", "#236FA6", "#005685", "#6b6a6a", "#454444", "#59B359", "#FFA217", "#CA4142"],
 
                     // custom selectors
                     // used for themed icons
                     customselectors: {
-                        "upload": [".icon_upload"],
-                        "connect-wellington": [".connect-wellington-logo"],
-                        "radio_button": [".icon_checkbox--unchecked, .icon_radio--unchecked, .field_checkbox__fancy, .field_radio__fancy"],
-                        "radio_button--checked": [".icon_checkbox--checked, .icon_radio--checked, .field_checkbox--checked .field_checkbox__fancy, .field_radio--checked .field_radio__fancy"],
-                        "caret-d": [".icon_disclosure"]
+                        "route-stop": [".route > .route__item:after"],
+                        "roadworks": [".route > .route__item.route__item--roadworks:after"],
+                        "warning": [".route > .route__item.route__item--warning:after"],
+                        "bg-hatched": [".breakline--hatched:before, .subnav__nav:before, .hatched-line:before, .level-1 .traffic-info__header:after"],
+                        "mask-traffic-figure": [".route .route__figure:after"]
                     },
 
-                    previewhtml: "icons.preview.html",
+                    previewhtml: "icons-preview.html",
 
                     // templates for css output and preview page
-                    template: "<%= paths.grunticons %>/template/default-css.hbs",
-                    previewTemplate:  "<%= paths.grunticons %>/template/preview.hbs"
+                    template: "<%= paths.grunticons %>/template/default-css.hbs"
+                    // , previewTemplate:  "<%= paths.grunticons %>/template/preview.hbs"
 
                 }
             }
         },
 
+        autoprefixer: {
 
-
-
-      /**
-         * Watch
-         */
-        watch: {
-            css: {
-                files: '<%= paths.assets %>/sass/{,*/}*.scss',
-                tasks: ['css'],
-                options: {
-                    livereload: true,
-                    atBegin: true
-                }
+            options: {
+                map: true,
+                browsers: ['> 1%', 'last 2 versions', 'ie >= 8', 'Android 2.3', 'Android 4', 'Firefox >= 4', 'iOS >= 5']
             },
 
-            j2: {
-                files: 'site/templates/**/*.j2',
-                tasks: ['noble_gas'],
+            // prefix the specified file
+            main: {
                 options: {
-                    livereload: true
-                }
-            },
-
-            js: {
-                files: [
-                    "<%= paths.javascript %>/**/*.js",
-                    "!<%= paths.javascript %>/dist/*.js"
-                ],
-                tasks: ['js'],
-                options: {
-                    livereload: true,
-                    atBegin: true
-                }
-            },
-
-            grunticon: {
-                files: ["<%= paths.assets %>/images/svg/**/*.svg"],
-                tasks: ["grunticon_pigment"],
-                options: {
-                    livereload: true
-                }
-            },
-
-            livereload: {
-                options: {
-                    livereload: 35729
                 },
-                files: [
-                    '<%= paths.site %>/*.html',
-                    '{.tmp,<%= paths.javascript %>}/{,*/}*.js',
-                    '<%= paths.assets %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}'
-                ]
+                src: 'www/css/screen.css'
             }
         },
 
-        // cssmin: {
-        //     pack: {
-        //         files: {
-        //             '<%= paths.css %>/screen.css': ['<%= paths.css %>/screen.css'],
-        //             '<%= paths.css %>/icons-data-png.css': ['<%= paths.css %>/icons-data-png.css'],
-        //             '<%= paths.css %>/icons-data-svg.css': ['<%= paths.css %>/icons-data-svg.css'],
-        //             '<%= paths.css %>/icons-fallback.css': ['<%= paths.css %>/icons-fallback.css']
-        //         }
-        //     }
-        // }
+        cssmin: {
+            dist: {
+                options: {
+                    banner: '/* mysuper */'
+                },
+                files: {
+                    'www/css/screen.css': ['www/css/screen.css']
+                }
+            }
 
-    });
+        },
 
-    // The cool way to load your grunt tasks
-    Object.keys( pkg.dependencies ).forEach( function( dep ){
-        if( dep.substring( 0, 6 ) === 'grunt-' ) grunt.loadNpmTasks( dep );
+        cmq: {
+            options: {
+                log: false
+            },
+            your_target: {
+                files: {
+                    'www/css/screen.css': ['www/css/screen.css']
+                }
+            }
+        }
+
     });
 
     /**
-     * Creates a file with all CSS imports
+     * The cool way to load your grunt tasks
      * --------------------------------------------------------------------
      */
-    grunt.registerMultiTask("crawl", "Create a file with all CSS assets", function() {
-        var paths = grunt.file.expand( this.data.paths ),
-            out = this.data.output,
-            contents = "", contentsCss = "", contentsCssImport = "";
-        paths.forEach(function( path ) {
-            contentsCss += '<link rel="stylesheet" type="text/css" href="' + path + '" />\n';
-            contentsCssImport += '@import url("' + path + '");\n';
-        });
-        contents += contentsCssImport + "\n\n" + contentsCss;
-        grunt.file.write( out, contents );
+    Object.keys( pkg.devDependencies ).forEach( function( dep ){
+        if( dep.substring( 0, 6 ) === 'grunt-' ) grunt.loadNpmTasks( dep );
     });
 
-    // Register tasks
+
 
     grunt.registerTask("css", [
         "sass",
-//        "cssmin"
+        "autoprefixer"
     ]);
 
-    grunt.registerTask("js", [
-//        "clean:js",
-        "jshint",
-        "concat:lib",
-        "browserify",
-        "concat:static_mappings",
-//        "uglify:dist",
-        "clean:build"
-    ]);
-
-    grunt.registerTask("images", [
-        //"imagemin",
-        //"svgmin"
-    ]);
-
-    grunt.registerTask("default", [
-        "sass",
-        "icon",
-        "js"
-    ]);
-
-    grunt.registerTask("dist", [
-        "build",
-        "uglify:dist"
-    ]);
-
-    grunt.registerTask("build", [
-        "checkDependencies",
-        "clean",
-        "images",
-        "sass",
-        "icon",
-        "js"
-    ]);
-
-    grunt.registerTask("noble_gas", function() {
-        return true;
-    })
 
 
     grunt.registerTask("icon", [
-      "grunticon_pigment"
+        "grunticon_pigment"
     ]);
 
 
-};
+    grunt.registerTask("dist", [
+        "css",
+        "cssmin:dist"
+    ]);
 
+
+    grunt.registerTask("all-the-things", [
+        "icon",
+        "css",
+        "cssmin:dist"
+    ]);
+
+    grunt.registerTask("all", ["all-the-things"]);
+    grunt.registerTask("boom", ["all-the-things"]);
+    grunt.registerTask("it-is-done", ["all-the-things"]);
+    grunt.registerTask("smash", ["all-the-things"]);
+
+};
